e81850a8b498e73af51cf245a4628797b938b4bb
DanielCLa98 <156672995+DanielCLa98@users.noreply.github.com>
2024-11-11 14:08:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-setInterval(() => {
-    const date = new Date();
-    console.log(date.toLocaleTimeString());
-}, 1000);
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Hola Mundo en JavaScript</title>
+</head>
+<body>
+    <h1 id="mensaje"></h1>
+    
+    <script>
+        // Selecciona el elemento con id "mensaje" y cambia su contenido
+        document.getElementById("mensaje").textContent = "Hola, Mundo";
+    </script>
+</body>
+</html>
